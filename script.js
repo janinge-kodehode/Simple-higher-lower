3f9e3b76cb44ca3e4d57c341d796d4fef7f4668c
janinge-kodehode <janinge.kodehode@gmail.com>
2025-06-16 13:09:26 +0200

    Simple higher lower

---
+++ script.js
@@ -0,0 +1,32 @@
+let x;
+let y;
+
+document.getElementById("diceBtn").onclick = function () {
+  x = Math.floor(Math.random() * 20) + 1;
+  document.getElementById("diceBtn").innerHTML = x;
+  console.log("Initial roll:", x);
+};
+
+document.getElementById("btn1").onclick = function () {
+  y = Math.floor(Math.random() * 20) + 1;
+  document.getElementById("diceBtn").innerHTML = y;
+  console.log("Higher?:", y);
+  if (y > x) {
+    document.getElementById("result").innerHTML = "You win!";
+  } else {
+    document.getElementById("result").innerHTML = "You lose!";
+  }
+  x = y;
+};
+
+document.getElementById("btn2").onclick = function () {
+  y = Math.floor(Math.random() * 20) + 1;
+  document.getElementById("diceBtn").innerHTML = z;
+  console.log("Lower:", z);
+  if (y < x) {
+    document.getElementById("result").innerHTML = "You win!";
+  } else {
+    document.getElementById("result").innerHTML = "You lose!";
+  }
+  x = y;
+};
